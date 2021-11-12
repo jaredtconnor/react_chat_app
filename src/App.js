2cab59b57f1cd341f35c1c8362af47ea8f4f02ea
Jared Connor <jaredconnor301@gmail.com>
2021-11-11 20:53:42 -0800

    Init but failing build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useSate } from 'react';
 import './App.css';
+
+import firebase from 'firebase/compat/app'; 
+import 'firebase/compat/firestore'; 
+import { } 'firebase/compat/auth'; 
+
+import { useAuthSate } from 'react-firebase-hooks/auth'; 
+import { useCollectionData } from 'react-firebase-hooks/firestore'; 
+
+const firebaseConfig = { 
+  //
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth(); 
+const firestore = firebase.firestore(); 
+
+const [user] = useAuthSate(auth); 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+
       </header>
+
+      <section> 
+        {user ? <ChatRoom /> : <LogIn />}
+      </section> 
+
+
     </div>
   );
 }
+
+function LogIn() { 
+
+  const signInWithGmail = () => { 
+    const provider = new firebase.auth.GoogleAuthProvider(); 
+    auth.signinWithPopup(provider); 
+  } 
+
+  return ( 
+    <button onClick={signInWithGmail}>Sign in with Google Account</button>
+  )
+}
+
+function LogOut() { 
+
+  const sign_out = auth.signOut()
+
+  return auth.currentUser && ( 
+    <button onClick={sign_out}>Sign Out</button>
+  )
+}
+
+function ChatRoom() { 
+
+  const messagesCollection = firestore.collection('messages'); 
+  const query = messagesCollection.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  return ( 
+    <> 
+      <div> 
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+
+
+    </>
+  )
+} 
+
+
+function ChatMessage(props) { 
+
+  const { text, uid } = props.message;
+
+  return ( 
+    <p>{text}</p>
+  )
+}
+
+
+
 
 export default App;
